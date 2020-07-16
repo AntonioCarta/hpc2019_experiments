8ec65859c785e2aa53c8a484cd0afa22860892b7
Antonio Carta <antonio.carta@di.unipi.it>
2020-07-16 12:22:04 +0200

    print cluster information

--- ray/container/demo_multinode.py
+++ ray/container/demo_multinode.py
@@ -7,16 +7,18 @@ import torch
 
 ray.init(address=os.environ["ip_head"], redis_password=os.environ["redis_password"])
 
+print("Ray nodes information:")
+print(ray.nodes())
 
-@ray.remote(num_cpus=10)
+@ray.remote(num_cpus=2)
 def f():
-    time.sleep(1)
+    time.sleep(5)
     return ray.services.get_node_ip_address()
 
-# The following takes one second (assuming that ray was able to access all of the allocated nodes).
+# Each iteration takes 25 second (assuming that ray was able to access all of the allocated nodes).
 for i in range(10):
     start = time.time()
-    ip_addresses = ray.get([f.remote() for _ in range(21)])
+    ip_addresses = ray.get([f.remote() for _ in range(56)])  # each node on hpc2019 has 56 cores.
     print(Counter(ip_addresses))
     end = time.time()
     print(end - start)
